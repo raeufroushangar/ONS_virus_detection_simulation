5725b90538f7fb47763dfd4baa498bb9bc90125a
raeufroushangar <roushnroushan19@gmail.com>
2022-11-16 22:31:35 +0000

    Submit job script

--- submit.sh
+++ submit.sh
@@ -8,7 +8,7 @@ source /etc/profile # Initialize the module command first
 module load anaconda/2020a # Load Anaconda and MPI module
 
 
-# MVM 3.08 x 10^2 virus to host ratio
+#  3.08 x 10^2 MVM virus to CHO host ratio
 python3 -B ont_virus_detection_simulation.py MVM 'Minute virus of mice.fasta' 50 'GCF_000223135.1_CriGri_1.0_genomic.fna' 1 '/home/raeuf/raeuf_notebook/bioinformatics_tools/apps/blast/db/RefSeq' 10power2 50 500 yes 10 24 3
 
 
